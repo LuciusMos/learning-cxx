2bd8f881938d8ff6e74b16211c0292f1000b5218
zhaoliangxuan <zhaolx_1999@qq.com>
2024-08-01 15:53:18 +0800

    try fixing 24, test

--- exercises/24_std_vector_bool/main.cpp
+++ exercises/24_std_vector_bool/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv) {
     ASSERT(vec[0], "Make this assertion pass.");
     ASSERT(vec[99], "Make this assertion pass.");
     ASSERT(vec.size() == 100, "Make this assertion pass.");
-    ASSERT(sizeof(vec) == 3 * sizeof(size_t), "Fill in the correct value.");
+    ASSERT(sizeof(vec) == 16, sizeof(vec));
     {
         vec[20] = false;
         ASSERT(!vec[20], "Fill in `vec[20]` or `!vec[20]`.");
