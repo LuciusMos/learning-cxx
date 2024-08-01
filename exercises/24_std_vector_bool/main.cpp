cabaeea67f412f278d26ea8099a483cc1a120a38
zhaoliangxuan <zhaolx_1999@qq.com>
2024-08-01 15:58:56 +0800

    fix 24

--- exercises/24_std_vector_bool/main.cpp
+++ exercises/24_std_vector_bool/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv) {
     ASSERT(vec[0], "Make this assertion pass.");
     ASSERT(vec[99], "Make this assertion pass.");
     ASSERT(vec.size() == 100, "Make this assertion pass.");
-    ASSERT(sizeof(vec) == 16, sizeof(vec));
+    ASSERT(sizeof(vec) == 5 * sizeof(size_t), sizeof(vec));// ISSUE: why 40?
     {
         vec[20] = false;
         ASSERT(!vec[20], "Fill in `vec[20]` or `!vec[20]`.");
